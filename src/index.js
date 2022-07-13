fc2c49f114b04cb6bce38834f0661e0464eb9030
fameAdmin <elonmucks@protonmail.com>
2022-07-14 01:28:19 +0530

    ⚡️ Breaking changes - Removed Resizer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 import React from 'react'
-import IframeResizer from 'iframe-resizer-react'
-import {nanoid} from 'nanoid'
+import { nanoid } from 'nanoid'
 
-const FamewallEmbed = ({ wallUrl, cardTheme, carouselMode=false }) => {
-  if (!wallUrl) {
-    console.error('Wall URL is required')
-    return null
+class FamewallEmbed extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { embedId: '' };
   }
-  const embedId = nanoid()
 
-  return (
-    <IframeResizer
-      title={`Famewall Embed ${wallUrl}`}
-      id={embedId}
-      src={cardTheme?`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}?theme=${cardTheme}`:`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}`}
-      frameBorder='0'
-      checkOrigin={false}
-      autoResize={true}
-      scrolling="no"
-      width='100%'
-      style={{
-        border: 0,
-        width: '1px',
-        minWidth: '100%'
-      }}
-    />
-  )
+  componentDidMount() {
+    if (!this.props.wallUrl) {
+      console.error('Wall URL is required')
+      return null
+    }
+
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.defer = true
+    s.src = "https://embed.famewall.io/frame.js";
+    this.instance.appendChild(s);
+    
+    const embedId = nanoid();
+    this.setState({ embedId: embedId });
+  }
+
+  render() {
+
+    const { wallUrl, cardTheme, carouselMode } = this.props;
+
+    return (
+      <div ref={el => (this.instance = el)} style={{width:'100%', height:'100%'}}>
+        <div class="famewall-embed" data-src={wallUrl} data-format={!carouselMode?"grid":"carousel"} data-theme={cardTheme==='dark'?'dark':'light'} style={{ width: "100%" }}></div>
+      </div>
+
+    )
+  }
 }
+
 
 export default FamewallEmbed

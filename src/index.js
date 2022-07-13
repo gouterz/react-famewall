a20c8b43613669c7d1362e94e84e7cee3fe205ad
fameAdmin <elonmucks@protonmail.com>
2022-07-14 02:09:10 +0530

    Reverted changes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
 import React from 'react'
-import { nanoid } from 'nanoid'
+import IframeResizer from 'iframe-resizer-react'
+import {nanoid} from 'nanoid'
 
-class FamewallEmbed extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { embedId: '' };
+const FamewallEmbed = ({ wallUrl, cardTheme, carouselMode=false }) => {
+  if (!wallUrl) {
+    console.error('Wall URL is required')
+    return null
   }
+  const embedId = nanoid()
 
-  componentDidMount() {
-    if (!this.props.wallUrl) {
-      console.error('Wall URL is required')
-      return null
-    }
-
-    const s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.defer = true
-    s.src = "https://embed.famewall.io/frame.js";
-    this.instance.appendChild(s);
-    
-    const embedId = nanoid();
-    this.setState({ embedId: embedId });
-  }
-
-  render() {
-
-    const { wallUrl, cardTheme, carouselMode } = this.props;
-
-    if(cardTheme==="dark"){
-      return (
-        <div ref={el => (this.instance = el)} style={{width:'100%', height:'100%'}}>
-          <div className="famewall-embed" data-src={wallUrl} data-format={!carouselMode?"grid":"carousel"} data-theme={'dark'} style={{ width: "100%" }}></div>
-        </div>
-  
-      )
-    }
-    else
-    return (
-      <div ref={el => (this.instance = el)} style={{width:'100%', height:'100%'}}>
-        <div className="famewall-embed" data-src={wallUrl} data-format={!carouselMode?"grid":"carousel"} data-theme={'light'} style={{ width: "100%" }}></div>
-      </div>
-
-    )
-  }
+  return (
+    <IframeResizer
+      title={`Famewall Embed ${wallUrl}`}
+      id={embedId}
+      src={cardTheme?`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}?theme=${cardTheme}`:`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}`}
+      frameBorder='0'
+      checkOrigin={false}
+      autoResize={true}
+      scrolling="no"
+      width='100%'
+      style={{
+        border: 0,
+        width: '1px',
+        minWidth: '100%'
+      }}
+    />
+  )
 }
-
 
 export default FamewallEmbed

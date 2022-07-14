5df62368d5be0ffbcd4d094bb80feed47cc74c0e
fameAdmin <elonmucks@protonmail.com>
2022-07-14 18:00:50 +0530

    🐛 Nanoid removed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import IframeResizer from 'iframe-resizer-react'
-import {nanoid} from 'nanoid'
 
 const FamewallEmbed = ({ wallUrl, cardTheme, carouselMode=false }) => {
   if (!wallUrl) {
     console.error('Wall URL is required')
     return null
   }
-  const embedId = nanoid()
 
   return (
     <IframeResizer
       title={`Famewall Embed ${wallUrl}`}
-      id={embedId}
       src={cardTheme?`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}?theme=${cardTheme}`:`https://wallembed.famewall.io/${!carouselMode?'wall':'carousel'}/${wallUrl}`}
       frameBorder='0'
       checkOrigin={false}
